92392535e0a5296fe0acbba3854eadabc2ea0045
Andre Oporto <dreoporto57@gmail.com>
2025-05-06 06:59:49 -0400

    add stats to retention logistic regression

--- chapter_6_logistic_regression/ex_6_17_confusion_matrix.py
+++ chapter_6_logistic_regression/ex_6_17_confusion_matrix.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import (confusion_matrix, f1_score, precision_score,
+                             recall_score)
 from sklearn.model_selection import train_test_split
+import misc.dre_helper as dh
 
 CSV_PATH = './chapter_6_logistic_regression/data/employee_retention_analysis.csv'
 RANDOM_SEED = 10
@@ -15,12 +17,25 @@ def main():
 
     model = LogisticRegression(solver='liblinear')
 
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=RANDOM_SEED)
-    model.fit(X_train, Y_train)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=RANDOM_SEED)
+    model.fit(X_train, y_train)
     prediction = model.predict(X_test)
 
-    matrix = confusion_matrix(y_true=Y_test, y_pred=prediction)
-    print(matrix)
+    tn, fp, fn, tp = confusion_matrix(y_true=y_test, y_pred=prediction).ravel()
+    dh.print_separator('CONFUSION MATRIX')
+    print(f'TP:{tp}\tFN:{fn}')
+    print(f'FP:{fp}\tTN:{tn}')
+    dh.print_separator()    
+
+    score = f1_score(y_true=y_test, y_pred=prediction)
+    print(f'F1 score:\t{score:0.4}')
+
+    p_score = precision_score(y_true=y_test, y_pred=prediction)
+    print(f'Precision:\t{p_score:0.4f}')
+
+    r_score = recall_score(y_true=y_test, y_pred=prediction)
+    print(f'Recall:\t\t{r_score:0.4f}')
+    dh.print_separator()
 
 if __name__ == '__main__':
     main()
